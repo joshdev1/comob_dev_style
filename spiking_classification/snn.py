8b93715d6fac20439c40495b8a373734b8be57b0
joshdev1 <32490908+joshdev1@users.noreply.github.com>
2022-08-26 19:34:47 +0100

    changed weight tensors to lower case w1 w2

--- spiking_classification/snn.py
+++ spiking_classification/snn.py
@@ -4,16 +4,16 @@ from parameters import num_hidden, batch_size, device, dtype, duration_steps, nu
 from spiking_classification.surrogate_gradient_descent import spike_fn
 
 
-def run_network(input_spikes, W1, W2):
-    s_rec = _input_to_hidden(input_spikes, W1)
-    return _hidden_to_output(s_rec, W2)
+def run_network(input_spikes, w1, w2):
+    s_rec = _input_to_hidden(input_spikes, w1)
+    return _hidden_to_output(s_rec, w2)
 
 
-def _input_to_hidden(input_spikes, W1):
+def _input_to_hidden(input_spikes, w1):
     v = torch.zeros((batch_size, num_hidden), device=device, dtype=dtype)
     s = torch.zeros((batch_size, num_hidden), device=device, dtype=dtype)
     s_rec = [s]
-    h = _update_weights_with_spikes(input_spikes, W1)
+    h = _update_weights_with_spikes(input_spikes, w1)
     for t in range(duration_steps - 1):
         new_v = (alpha * v + h[:, t, :]) * (1 - s)
         s = spike_fn(v - 1)
@@ -22,10 +22,10 @@ def _input_to_hidden(input_spikes, W1):
     return s_rec
 
 
-def _hidden_to_output(s_rec, W2):
+def _hidden_to_output(s_rec, w2):
     v = torch.zeros((batch_size, num_classes), device=device, dtype=dtype)
     v_rec = [v]
-    h = _update_weights_with_spikes(torch.stack(s_rec, dim=1), W2)
+    h = _update_weights_with_spikes(torch.stack(s_rec, dim=1), w2)
     for t in range(duration_steps - 1):
         v = alpha * v + h[:, t, :]
         v_rec.append(v)
